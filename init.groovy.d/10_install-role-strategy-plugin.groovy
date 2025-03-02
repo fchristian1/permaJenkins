91909ad085f5ee8342689d2a1ceb447e02e002eb
fchristian1 <fchristian1@gmx.net>
2025-03-03 00:20:31 +0100

    Verbessere die Plugin-Installationslogik, um mehrere Plugins zu installieren und deren Verfügbarkeit zu überprüfen

--- init.groovy.d/10_install-role-strategy-plugin.groovy
+++ init.groovy.d/10_install-role-strategy-plugin.groovy
@@ -9,23 +9,28 @@ def updateCenter = instance.getUpdateCenter()
 // Warten, bis das Update Center vollständig initialisiert ist
 updateCenter.updateAllSites()
 
-def plugin = pluginManager.getPlugin("role-strategy")
-if (plugin == null) {
-    logger.info("Installing 'role-strategy' plugin...")
-    def pluginDeployment = null
-    while (pluginDeployment == null) {
-        def pluginInfo = updateCenter.getPlugin("role-strategy")
-        if (pluginInfo != null) {
-            pluginDeployment = pluginInfo.deploy()
-        } else {
-            logger.info("Waiting for 'role-strategy' plugin to become available...")
-            sleep(10000) // 10 Sekunden warten
+def pluginsToInstall = ["caffeine-api", "commons-lang3-api", "ionicons-api", "role-strategy"]
+def installedPlugins = pluginManager.plugins.collect { it.getShortName() }
+
+pluginsToInstall.each { pluginName ->
+    if (!installedPlugins.contains(pluginName)) {
+        logger.info("Installing '${pluginName}' plugin...")
+        def pluginDeployment = null
+        while (pluginDeployment == null) {
+            def pluginInfo = updateCenter.getPlugin(pluginName)
+            if (pluginInfo != null) {
+                pluginDeployment = pluginInfo.deploy()
+            } else {
+                logger.info("Waiting for '${pluginName}' plugin to become available...")
+                sleep(10000) // 10 Sekunden warten
+            }
         }
+        pluginDeployment.get()
+        logger.info("'${pluginName}' plugin installed.")
+    } else {
+        logger.info("'${pluginName}' plugin is already installed.")
     }
-    pluginDeployment.get()
-    logger.info("'role-strategy' plugin installed.")
-    instance.save()
-    instance.restart()
-} else {
-    logger.info("'role-strategy' plugin is already installed.")
 }
+
+instance.save()
+instance.restart()
