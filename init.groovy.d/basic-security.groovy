54cf43ee5f190d74055e80ffdbd391b11182e0d4
fchristian1 <fchristian1@gmx.net>
2025-03-02 23:46:26 +0100

    Überprüfe und installiere das 'role-strategy' Plugin bei der Initialisierung

--- init.groovy.d/basic-security.groovy
+++ init.groovy.d/basic-security.groovy
@@ -1,8 +1,26 @@
 import jenkins.model.*
 import hudson.security.*
 import com.michelin.cio.hudson.plugins.rolestrategy.*
+import java.util.logging.Logger
 
+def logger = Logger.getLogger("")
 def instance = Jenkins.getInstance()
+def pluginManager = instance.getPluginManager()
+def updateCenter = instance.getUpdateCenter()
+
+// Überprüfen, ob das 'role-strategy' Plugin installiert ist
+def plugin = pluginManager.getPlugin("role-strategy")
+if (plugin == null) {
+    logger.info("Installing 'role-strategy' plugin...")
+    def pluginDeployment = updateCenter.getPlugin("role-strategy").deploy()
+    pluginDeployment.get()
+    logger.info("'role-strategy' plugin installed.")
+    instance.save()
+    instance.restart()
+    return
+} else {
+    logger.info("'role-strategy' plugin is already installed.")
+}
 
 // Sicherheitsrealm setzen (Benutzer und Passwort)
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
