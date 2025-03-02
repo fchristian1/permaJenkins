9153e897a9184ddb974ab87f5f82f71e576fabfb
fchristian1 <fchristian1@gmx.net>
2025-03-02 23:39:55 +0100

    Setze rollenbasierte Berechtigungen für den Admin-Benutzer und verbessere die Benutzerüberprüfung

--- init.groovy.d/basic-security.groovy
+++ init.groovy.d/basic-security.groovy
@@ -1,36 +1,37 @@
-#!groovy
-
 import jenkins.model.*
 import hudson.security.*
-import hudson.util.*
 import jenkins.install.*
 import com.michelin.cio.hudson.plugins.rolestrategy.*
+import hudson.model.User
 
 def instance = Jenkins.getInstance()
 
-// Überprüfen, ob der Admin-Benutzer bereits existiert
-def hudsonRealm = instance.getSecurityRealm()
-def adminUser = hudsonRealm.getUser("admin")
+// Sicherheitsrealm setzen (Benutzer und Passwort)
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 instance.setSecurityRealm(hudsonRealm)
+
+// Überprüfen, ob der Admin-Benutzer bereits existiert
+def adminUser = User.get("admin", false, null)
+
+if (adminUser == null) {
+    hudsonRealm.createAccount("admin", "admin")
+    println("Admin-User 'admin' mit Passwort 'admin' wurde erfolgreich erstellt!")
+} else {
+    println("Admin-User 'admin' existiert bereits. Keine Änderungen vorgenommen.")
+}
 
 // Rollenbasierte Berechtigungen setzen
 def roleBasedStrategy = new RoleBasedAuthorizationStrategy()
 instance.setAuthorizationStrategy(roleBasedStrategy)
 
-if (adminUser == null) {
-    // Sicherheitsrealm setzen (Benutzer und Passwort)
-    hudsonRealm = new HudsonPrivateSecurityRealm(false)
-    hudsonRealm.createAccount("admin", "admin")
+// Admin-Rolle erstellen und Berechtigungen zuweisen
+def permissions = [
+    hudson.model.Hudson.ADMINISTER,
+    // Weitere Berechtigungen können hier hinzugefügt werden
+]
+def adminRole = new Role("admin", ".*", permissions)
+roleBasedStrategy.addRole(RoleBasedAuthorizationStrategy.GLOBAL, adminRole)
+roleBasedStrategy.assignRole(RoleBasedAuthorizationStrategy.GLOBAL, adminRole, Collections.singleton("admin"))
 
-    // Admin-Rolle erstellen und Berechtigungen zuweisen
-    def adminRole = new Role("admin", ".*", [Permission.all()])
-    roleBasedStrategy.addRole(RoleBasedAuthorizationStrategy.GLOBAL, adminRole)
-    roleBasedStrategy.assignRole(RoleBasedAuthorizationStrategy.GLOBAL, adminRole, "admin")
-
-    // Änderungen speichern
-
-    println("Admin-User 'admin' mit Passwort 'admin' wurde erfolgreich erstellt und Rollenbasierte Berechtigungen gesetzt!")
-} else {
-    println("Admin-User 'admin' existiert bereits. Keine Änderungen vorgenommen.")
-}
+// Änderungen speichern
 instance.save()
