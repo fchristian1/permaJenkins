a4bd3a2acf089bc3d60bc7aaf7db9a736871c21d
fchristian1 <fchristian1@gmx.net>
2025-03-03 08:55:29 +0100

    Füge newgrp docker hinzu, um die Gruppenänderung sofort anzuwenden

--- start.sh
+++ start.sh
@@ -18,6 +18,8 @@ if [ $docker_install -eq 1 ]; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
     sudo usermod -aG docker $USER
+    newgrp docker
+
 fi
 
 docker compose up -d
