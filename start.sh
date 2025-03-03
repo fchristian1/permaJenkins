d83c6c7972186616f72214e4449cfbea5ed5651e
fchristian1 <fchristian1@gmx.net>
2025-03-03 08:48:08 +0100

    Füge Überprüfungen für Docker- und Docker Compose-Installationen im Startskript hinzu

--- start.sh
+++ start.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+
+docker_install=0
+
+#check docker and docker compose installation
+if ! command -v docker &>/dev/null; then
+    echo "Docker is not installed. Please install Docker first."
+    docker_install=1
+fi
+
+if ! command -v docker compose &>/dev/null; then
+    echo "Docker Compose is not installed. Please install Docker Compose first."
+    docker_install=1
+fi
+
+if [ $docker_install -eq 1 ]; then
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh --dry-run
+fi
+
 docker compose up -d
