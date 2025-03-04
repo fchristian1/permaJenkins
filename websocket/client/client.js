012352df842f34e52272615857bd096943ba01f1
fchristian1 <fchristian1@gmx.net>
2025-03-04 10:56:51 +0100

    Aktualisiere WebSocket-Client: Füge WEBSOCKET_URL zur Umgebungsvariablen hinzu und verbessere Logging mit Zeitstempeln

--- websocket/client/client.js
+++ websocket/client/client.js
@@ -6,27 +6,28 @@ config();
 const JENKINS_URL = process.env.JENKINS_URL ?? console.error('JENKINS_URL not found in .env file');
 const JENKINS_USER = process.env.JENKINS_USER ?? console.error('JENKINS_USER not found in .env file');
 const JENKINS_TOKEN = process.env.JENKINS_TOKEN ?? console.error('JENKINS_TOKEN not found in .env file');
+const WEBSOCKET_URL = process.env.WEBSOCKET_URL ?? console.error('WEBSOCKET_URL not found in .env file');
 
 const ws = connect();
 
 // Intervallfunktion zum Verbinden mit dem Server
 function connect() {
-    const ws = new WebSocket('ws://85.209.49.32:28080');
-    ws.on('error', console.error);
+    const ws = new WebSocket(WEBSOCKET_URL);
+    ws.on("[" + Date.now() + "] [ERROR]", console.error);
     ws.on('open', function open() {
-        console.log('connected');
+        console.log("[" + Date.now() + "] [INFO]", 'connected');
         ws.send('something');
     });
     ws.on('message', function message(data) {
-        console.log('received: %s', data.toString().includes('githubTrigger'));
+        console.log("[" + Date.now() + "] [INFO]", 'received: %s', data.toString().includes('githubTrigger'));
 
         if (data.toString().includes('githubTrigger') && JSON.parse(data.toString()).type == 'githubTrigger') {
-            console.log('githubTrigger', Date.now());
+            console.log("[" + Date.now() + "] [INFO]", 'githubTrigger');
             sendToJenkins(JSON.parse(data.toString()).payload, JSON.parse(data.toString()).headers);
         }
     });
     ws.on('close', function close() {
-        console.log('disconnected: ' + Date.now());
+        console.log("[" + Date.now() + "] [INFO]", 'disconnected: ' + Date.now());
         setTimeout(connect, 1000);
     });
     return ws;
@@ -67,12 +68,12 @@ async function sendToJenkins(webhookData, headers) {
         //console.log('url: ', `${JENKINS_URL}/generic-webhook-trigger/invoke`, 'fetchData:', JSON.stringify(webhookData, null, 2));
         const response = await fetch(`${JENKINS_URL}/generic-webhook-trigger/invoke`, fetchData);
         if (!response.ok) {
-            console.log('Error:', await response.text());
+            console.log("[" + Date.now() + "] [ERROR]", await response.text());
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        console.log('Success send Data to Jenkins:', response);
+        console.log("[" + Date.now() + "] [INFO]", 'Success send Data to Jenkins');
     } catch (error) {
-        console.error('Error:', error);
+        console.error("[" + Date.now() + "] [ERROR]", error);
     }
 
 }
